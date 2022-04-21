ff481faa8db8dcaebd88ef9eb0e3827d898b4425
Junior Mbogning <juniormbogning@gmail.com>
2022-04-21 07:49:08 +0100

    mise en place du side menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Fragment } from 'react';
+import { Header } from './components/Header';
+import SideMenu from './components/SideMenu';
+import './styles/App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        accueil
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+      <div className='container mt-5'>
+        <div className='row'>
+          <SideMenu />
+          <div className='col-sm'>
+            store
+          </div>
+        </div>
       </div>
-    );
-  }
+    </Fragment>
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../styles/App.css'
+
+
+export const Header = () => {
+    return (
+        <nav className='navbar orange navbar-expand-lg navbar-light bg-light'>
+            <a href='' className='navbar-brand crimson'>
+                <i className='fas fa-shopping-cart'> My-SHop</i>
+            </a>
+            <button
+                className='navbar-toggler'
+                type='button'
+                data-toggle='collapse'
+                data-target='navbarSupportedContent'
+                aria-controls='navbarSupportedContent'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
+            >
+                <span className='navbar-toggler-icon'></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    
+                </ul>
+                <form className="d-flex">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Header
